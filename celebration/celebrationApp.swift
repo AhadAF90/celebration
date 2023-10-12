42facdd521f5bd888daae21ac2f05bdb7ddd9f0c
Ahad <ahoudtiion@hotmail.com>
2023-10-12 09:56:52 +0300

    Initial Commit

---
+++ celebration/celebrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  celebrationApp.swift
+//  celebration
+//
+//  Created by Ahad on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct celebrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
